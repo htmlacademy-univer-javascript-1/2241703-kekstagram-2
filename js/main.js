a138a2edae25009d80006010560139b3d56ebf6e
polinann <127510683+polinann@users.noreply.github.com>
2023-06-22 21:19:40 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
- function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min
+function getRandomNumber(min, max) {
+  if (min < max && min >= 0 && max >= 0) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+  if (min >= max) {
+    return 'Incorrect value entry: minimum is greater than maximum';
+     }
+    if (max < 0 || min < 0) {
+        throw new RangeError('Minimum and maximum values must be positive');
+    }
   }
-console.log(getRandomNumber)
+
+
+const checkMaxSymbol = (str, maxLength) => {
+    return str.length <= maxLength;
+}
+
+  "dependencies": {},
+  "scripts": {
+    "start": "browser-sync start --server --no-ui --files \"js/**/*.js\"",
+    "lint": "eslint js/",
+    "lintfix": "eslint js/ --fix"
+  },
+  "engines": {
+    "node": "16.x",
